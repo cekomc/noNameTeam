8a0abe5521033ebc28ef7376e50085eed27bd082
Nikola Ivanov <nikola4421@gmail.com>
2017-06-15 15:39:12 +0300

    Task 4-1 index.html & prodavalnic.js changes

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,6 +1,9 @@
 function startApp() {
-
-
+	
+    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
 
     showHideMenuLinks();
     showHomeView();
@@ -38,12 +41,14 @@ function startApp() {
             $("#linkRegister").show();
 			$("#linkListAds").hide();
             $("#linkLogout").hide();
+            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
 		    $("#linkListAds").show();
             $("#linkLogout").show();
+			$("#loggedInUser").show();
         }
     }
 
@@ -95,6 +100,10 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
+		let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
+
     }
 
     // user/register
